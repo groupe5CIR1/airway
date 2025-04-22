05736d721c2ca5c495791281366ae86535afb40b
nicolas24 <vandonselaarn@gmail.com>
2025-04-22 11:06:40 +0200

    noCopy

---
+++ js/general.js
@@ -0,0 +1,4 @@
+function noCopy() {
+    console.log("“Toute édition d'écrits, de composition musicale, de dessin […] imprimée ou gravée en entier ou en partie au mépris des lois et règlements relatifs à la propriété des auteurs est une contrefaçon et un délit […]” Art. L335-2 du Code de la propriété intellectuelle.")
+};
+
