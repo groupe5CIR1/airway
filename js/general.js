9773efb96d64ceb7c2b7dd5838efe4984d64bb01
nicolas24 <vandonselaarn@gmail.com>
2025-05-06 15:59:34 +0200

    equipe validation

--- js/general.js
+++ js/general.js
@@ -2,3 +2,10 @@ function noCopy() {
     console.log("“Toute édition d'écrits, de composition musicale, de dessin […] imprimée ou gravée en entier ou en partie au mépris des lois et règlements relatifs à la propriété des auteurs est une contrefaçon et un délit […]” Art. L335-2 du Code de la propriété intellectuelle.")
 };
 
+function equipe(){
+    const conf = confirm("Souhaitez-vous continuer ?");
+    if (conf == true) {
+        open("equipe.html");
+        
+    };
+};
