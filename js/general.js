bdd5e84e9687ab802f2e350073af5422cd5af6f2
door_maker <tangpaturelvalentin@gmail.com>
2025-05-20 12:04:36 +0200

    timer truc jsp

--- js/general.js
+++ js/general.js
@@ -14,14 +14,19 @@ function updateClock() {
     document.getElementById('clock').textContent = time;
   }
 
-// Chronomètre
-let secondsElapsed = 0;
-function updateTimer() {
-  secondsElapsed++;
-  document.getElementById('timer').textContent = secondsElapsed + 's';
+
+function timer() {
+    // Chronomètre
+    let secondsElapsed = 0;
+    function updateTimer() {
+        secondsElapsed++;
+        document.getElementById('timer').textContent = secondsElapsed + 's';
+    }
+
+    // Démarrage
+    updateClock();
+    setInterval(updateClock, 1000); // met à jour chaque seconde
+    setInterval(updateTimer, 1000); // incrémente chaque seconde
 }
 
-// Démarrage
-updateClock();
-setInterval(updateClock, 1000); // met à jour chaque seconde
-setInterval(updateTimer, 1000); // incrémente chaque seconde
+timer();
