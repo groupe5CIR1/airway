aff5aeda0e334c2bc69e971037ec3697a09a706e
nicolas24 <vandonselaarn@gmail.com>
2025-05-27 10:26:58 +0200

    ajout de commentaires

--- js/equipe.js
+++ js/equipe.js
@@ -23,20 +23,20 @@ function setupScratchCard(name) {
     const ctx = canvas.getContext('2d');
     const image = document.querySelector(`.image-${name}`);
     
-    // Dimensionne le canvas à l'image
+    // Recupere les dimensions de l'image et la met pour le canvas
     canvas.width = image.offsetWidth;
     canvas.height = image.offsetHeight;
     
     // Crée la couche à gratter
     ctx.fillStyle = 'gray';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.globalCompositeOperation = 'destination-out';
+    ctx.fillRect(0, 0, canvas.width, canvas.height); 
+    ctx.globalCompositeOperation = 'destination-out'; // effacer et pas ajouter
     
     const radius = 20;
     
     // Grattage au survol
     canvas.addEventListener('mousemove', (e) => {
-        const rect = canvas.getBoundingClientRect();
+        const rect = canvas.getBoundingClientRect(); //pour que la fonction soit sur le canvas et pas sur la page
         const x = e.clientX - rect.left;
         const y = e.clientY - rect.top;
         
@@ -45,9 +45,9 @@ function setupScratchCard(name) {
         ctx.fill();
     });
 
-    // Support tactile
+    // Support tactile pour telephone
     canvas.addEventListener('touchmove', (e) => {
-        e.preventDefault();
+        e.preventDefault(); // eviter les touchs indesirables
         const rect = canvas.getBoundingClientRect();
         const x = e.touches[0].clientX - rect.left;
         const y = e.touches[0].clientY - rect.top;
